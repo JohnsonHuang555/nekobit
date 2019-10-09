73b31dd4db0eaf56cd9733f739a09da322033276
DESKTOP-E65KLJK\User <imchung0707@gmail.com>
2019-10-10 02:14:59 +0800

    新增controller

---
+++ server/controllers/gameController.go
@@ -0,0 +1,35 @@
+package gamecontroller
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// GetAllGames from the DB and return it
+func GetAllGames(collection *mongo.Collection) []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		results = append(results, result)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.Background())
+	return results
+}
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -7,10 +7,10 @@ import (
 	"log"
 	"net/http"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	gamecontroller "../controllers"
 )
 
 // DB connection string
@@ -60,31 +60,6 @@ func init() {
 func GetAllGames(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	payload := getAllGames()
+	payload := gamecontroller.GetAllGames(collection)
 	json.NewEncoder(w).Encode(payload)
-}
-
-// get all task from the DB and return it
-func getAllGames() []primitive.M {
-	cur, err := collection.Find(context.Background(), bson.D{{}})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var results []primitive.M
-	for cur.Next(context.Background()) {
-		var result bson.M
-		e := cur.Decode(&result)
-		if e != nil {
-			log.Fatal(e)
-		}
-		results = append(results, result)
-	}
-
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	cur.Close(context.Background())
-	return results
 }
