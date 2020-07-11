ce4608f77b8604a7dad1929bf315a389b0322cb9
DESKTOP-E65KLJK\User <imchung0707@gmail.com>
2020-07-11 15:37:00 +0800

    fix gameDetail css

--- client/src/features/main/game/components/GameDetail.tsx
+++ client/src/features/main/game/components/GameDetail.tsx
@@ -9,6 +9,8 @@ import {
   faHome
 } from '@fortawesome/free-solid-svg-icons';
 import { TGame } from 'src/features/main/domain/models/Game';
+import { Grid, Box, Button } from '@material-ui/core';
+import styles from '@styles/components/gameDetail.module.scss';
 
 type GameDetailProps = {
   gameInfo: TGame;
@@ -26,11 +28,54 @@ const GameDetail = (props: GameDetailProps) => {
   } = props;
 
   return (
-    <>
+    <Box className={styles.gameDetail}>
       <div className="section-heading">
         <h2>遊戲介紹</h2>
       </div>
-      <div className="row main">
+      <Grid container spacing={3}>
+        <Grid item xs={7}>
+          <img src={`${gameInfo.imgUrl}/game.png`} alt={gameInfo.name} width="100%" />
+        </Grid>
+        <Grid item xs={5}>
+          <Box className={styles.gameName}>{gameInfo.name}</Box>
+          <Box className={styles.description}>{gameInfo.description}</Box>
+          <Grid container className={styles.info}>
+            <Grid item xs={6}>
+              <FontAwesomeIcon icon={faUserFriends} className={styles.icon} />
+              <span>遊戲需求人數：</span>
+              <span>{gameInfo.maxPlayers}</span>
+            </Grid>
+            <Grid item xs={6}>
+              <FontAwesomeIcon icon={faChess} className={styles.icon} />
+              <span>總房間數：</span>
+              <span>{roomsCount}</span>
+            </Grid>
+          </Grid>
+          <Box>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={
+                <FontAwesomeIcon icon={faDoorOpen} />
+              }
+            >
+              New Room
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={
+                <FontAwesomeIcon icon={faGamepad} />
+              }
+            >
+              PlayNow
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+      {/* <div className="row main">
           <div className="col-md-7 game-image">
             <img src={`${gameInfo.imgUrl}/game.png`} alt={gameInfo.name} width="100%" />
           </div>
@@ -64,8 +109,8 @@ const GameDetail = (props: GameDetailProps) => {
               <span>回遊戲大廳</span>
             </div>
           </div>
-        </div>
-    </>
+        </div> */}
+    </Box>
   );
 };
 
