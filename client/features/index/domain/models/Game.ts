6802ead625dae41b7eedf60321ecfdd1a2669f65
DESKTOP-E65KLJK\User <imchung0707@gmail.com>
2020-11-10 23:29:05 +0800

    add swiper

---
+++ client/features/index/domain/models/Game.ts
@@ -0,0 +1,12 @@
+export type Game = {
+  id: string;
+  name: string;
+  minPlayers: number;
+  maxPlayers: number;
+  brief: string;
+  description: string;
+  imgUrl: string;
+  estimateTime: number;
+  createAt: string; // FIXME:
+  updateAt: string; // FIXME:
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,29 +1,65 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Layout from 'components/Layout';
 import styles from 'styles/components/home.module.scss';
+import { Game } from 'features/index/domain/models/Game';
+
+const testData: Game[] = [
+  {
+    id: '123-555',
+    name: '象棋',
+    minPlayers: 2,
+    maxPlayers: 2,
+    brief: '對弈',
+    description: 'PKPK',
+    imgUrl: '/img/chinese-chess',
+    estimateTime: 30,
+    createAt: '',
+    updateAt: '',
+  },
+  {
+    id: '888-555',
+    name: '西洋棋',
+    minPlayers: 2,
+    maxPlayers: 2,
+    brief: '對弈',
+    description: 'PKPK',
+    imgUrl: '/img/chess',
+    estimateTime: 30,
+    createAt: '',
+    updateAt: '',
+  },
+]
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!2222</a>
-      </h1>
-
-      <p className={styles.description}>
-        Get started by editing{' '}
-        <code className={styles.code}>pages/index.js</code>
-      </p>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+      <div
+        className={styles.backgroundImage}
+        style={{ backgroundImage: "url('img/intro.jpg')" }}
+      />
+      <div className={styles.container}>
+        <div className={styles.info}>
+          <h1>熱門遊戲</h1>
+          <p>歡迎來到 Gplay ，免費遊玩各式桌遊喔 ^^</p>
+        </div>
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={6}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {testData.map(game => (
+            <SwiperSlide>
+              <img
+                src={`${game.imgUrl}/home.png`}
+                alt={game.name}
+                width="100%"
+                height="100%"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </Layout>
   )
