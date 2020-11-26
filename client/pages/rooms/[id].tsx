a33c8e1533d51044acf118f63874b8446b1abafe
DESKTOP-E65KLJK\User <imchung0707@gmail.com>
2020-11-27 01:43:56 +0800

    add room scss

--- client/pages/rooms/[id].tsx
+++ client/pages/rooms/[id].tsx
@@ -1,11 +1,20 @@
-import Layout from 'components/Layout';
 import React from 'react';
+import Button from 'components/Button';
+import Layout from 'components/Layout';
+import styles from 'styles/pages/rooms.module.scss';
 
 const Room = () => {
   return (
     <Layout>
-      <div className="header">
-        <h2 className="page-title">象棋</h2>
+      <div className={styles.mainArea}>
+        <div className={styles.leftArea}>
+          <div className={styles.block}>123</div>
+          <div className={styles.block}>333</div>
+        </div>
+        <div className={`${styles.rightArea} ${styles.block}`}>
+          <Button title="開始遊戲" />
+          <Button title="離開房間" />
+        </div>
       </div>
     </Layout>
   )
