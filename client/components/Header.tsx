deb9a5f7f76cbffe777e0808931a027d1f3f1b8a
DESKTOP-E65KLJK\User <imchung0707@gmail.com>
2020-12-06 15:16:33 +0800

    新增訪客登入

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Icon, { IconType } from './Icon';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectShowModal, selectUserInfo } from 'selectors/appSelector';
 import Modal from './Modal';
-import styles from 'styles/components/header.module.scss';
 import Input from './Input';
 import Button from './Button';
-import { setShowModal } from 'slices/appSlice';
+import { setShowModal, setUserInfo } from 'slices/appSlice';
+import { v4 as uuidv4 } from 'uuid';
+import styles from 'styles/components/header.module.scss';
+import { User } from 'domain/models/User';
 
 const Header = () => {
   const dispatch = useDispatch();
   const { userInfo } = useSelector(selectUserInfo);
   const { showModal } = useSelector(selectShowModal);
   const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const user = localStorage.getItem('userInfo');
+    if (user) {
+      const uiObj: User = JSON.parse(user);
+      dispatch(setUserInfo(uiObj));
+    }
+  }, [dispatch])
+
+  const onLogin = () => {
+    const user: User = {
+      id: uuidv4(),
+      name: userName,
+    };
+    localStorage.setItem('userInfo', JSON.stringify(user));
+    dispatch(setUserInfo(user));
+    dispatch(setShowModal(false));
+  };
 
   return (
     <>
@@ -26,7 +45,11 @@ const Header = () => {
           onChange={(e) => setUserName(e.target.value)}
           customStyles={{ marginBottom: '20px' }}
         />
-        <Button title="確認" color="secondary" onClick={() => {}} />
+        <Button
+          title="確認"
+          color="secondary"
+          onClick={() => onLogin()}
+        />
       </Modal>
       <header className={styles.header}>
         <Link href="/">
@@ -51,18 +74,19 @@ const Header = () => {
         </ul>
         {userInfo ?
           <>
-            <div className={styles.alert}>
+            {/* beta 版不做 */}
+            {/* <div className={styles.alert}>
               <Icon type={IconType.Alert} />
-            </div>
+            </div> */}
             <div className={styles.userInfo}>
               <img
-                src="https://images.pexels.com/photos/3541389/pexels-photo-3541389.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                src="https://images.pexels.com/photos/4297820/pexels-photo-4297820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
                 alt="user-avator"
                 width={46}
                 height={46}
               />
               <div className={styles.detail}>
-                <span className={styles.userName}>瑪姬阿米是誰</span>
+                <span className={styles.userName}>{userInfo.name}</span>
                 <span className={styles.userLevel}>Lv. 100</span>
               </div>
             </div>
@@ -72,7 +96,12 @@ const Header = () => {
               <div className={styles.logIn}>Log In</div>
             </li>
             <li>
-              <div className={styles.getStart} onClick={() => dispatch(setShowModal(true))}>Get Start</div>
+              <div
+                className={styles.getStart}
+                onClick={() => dispatch(setShowModal(true))}
+              >
+                Get Start
+              </div>
             </li>
           </ul>
         }
