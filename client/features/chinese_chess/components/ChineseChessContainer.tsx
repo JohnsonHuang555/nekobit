8424231e336956a834aac9d360b2f7ddf0d0d885
Johnson Huang <imchung0707@gmail.com>
2020-12-17 18:08:51 +0800

    改點 css

--- client/features/chinese_chess/components/ChineseChessContainer.tsx
+++ client/features/chinese_chess/components/ChineseChessContainer.tsx
@@ -2,10 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectGameData } from "../selectors/chineseChessSelector";
 import GameMap from "./GameMap";
 import styles from 'styles/features/chineseChess.module.scss';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setGameData } from "../slices/chineseChessSlice";
 import { selectRoomInfo } from "selectors/roomsSelector";
-import { ChessSide } from "../domain/models/ChineseChess";
+import { ChessSide, ChineseChess } from "../domain/models/ChineseChess";
 import { wsSendMessage } from "actions/socketAction";
 import { ChineseChessSocketEvent } from "domain/models/WebSocket";
 import { selectUserInfo } from "selectors/appSelector";
@@ -15,16 +15,20 @@ const ChineseChessContainer = () => {
   const { chineseChess, playerSide } = useSelector(selectGameData);
   const { userInfo } = useSelector(selectUserInfo);
   const { selectedRoom } = useSelector(selectRoomInfo);
+  const [selectedChess, setSelectedChess] = useState<ChineseChess>();
 
-  if (!userInfo) {
+  if (!userInfo || !selectedRoom) {
     return null;
   }
 
   useEffect(() => {
     if (!chineseChess.length) {
-      dispatch(setGameData(selectedRoom?.gameData));
+      dispatch(setGameData(selectedRoom.gameData));
     }
-  }, [])
+  }, []);
+
+  const isYourTurn = userInfo.id === selectedRoom.nowTurn ? true : false;
+  const yourSide = playerSide[userInfo.id];
 
   const chessMap = () => {
     let map = [];
@@ -41,12 +45,12 @@ const ChineseChessContainer = () => {
         };
 
         const onChessClick = () => {
-          // if (!chessInfo || !yourTurn) { return; }
-          // if (playerSide === chessInfo.side) {
-          //   onSelect(chessInfo);
-          // } else if (selectedChess && selectedChess.side !== chessInfo.side) {
-          //   onEat(chessInfo);
-          // }
+          if (!chess || !isYourTurn) { return; }
+          if (yourSide === chess.side) {
+            setSelectedChess(chess);
+          } else if (selectedChess && selectedChess.side !== chess.side) {
+            // onEat(chessInfo);
+          }
         };
 
         const isSelected = (): boolean => {
@@ -62,18 +66,27 @@ const ChineseChessContainer = () => {
           map.push(
             <div className={styles.itemContainer} key={`x-${x}/y-${y}`}>
               {chess.isFliped ?
-                <span className={`${styles.flipedChess} ${chess.side === ChessSide.Black ? styles.black : styles.red}`}>
-                  <span>{chess.name}</span>
-                  <span className={`${styles.circle} ${chess.side === ChessSide.Red ? styles.red : ''}`}></span>
-                </span> :
-                <span className={styles.notFlipedChess} onClick={() => dispatch(wsSendMessage({
-                  event: ChineseChessSocketEvent.FlipChess,
-                  player_id: userInfo.id,
-                  data: {
-                    chess_id: chess.id,
-                    chinese_chess_side: chess.side,
-                  }
-                }))}></span>
+                (
+                  <span
+                    className={`${styles.flipedChess} ${chess.side === ChessSide.Black ? styles.black : styles.red}`}
+                    onClick={() => onChessClick()}
+                  >
+                    <span>{chess.name}</span>
+                    <span className={`${styles.circle} ${chess.side === ChessSide.Red ? styles.red : ''}`}></span>
+                  </span>
+                ) : (
+                  <span
+                    className={styles.notFlipedChess}
+                    onClick={() => dispatch(wsSendMessage({
+                      event: ChineseChessSocketEvent.FlipChess,
+                      player_id: userInfo.id,
+                      data: {
+                        chess_id: chess.id,
+                        chinese_chess_side: chess.side,
+                      }
+                    }))}
+                  />
+                )
               }
             </div>
           )
@@ -82,7 +95,6 @@ const ChineseChessContainer = () => {
             <div className={styles.itemContainer} key={`x-${x}/y-${y}`}></div>
           )
         }
-
       }
     }
     return map;
@@ -90,12 +102,29 @@ const ChineseChessContainer = () => {
 
   return (
     <div className={styles.chineseChess}>
-      <div className={styles.map}>
-        <GameMap />
-        <div className={styles.chesses}>
-          {chessMap()}
+      <div className={styles.header}>
+        <div className={styles.players}>
+          <span className={styles.name}>{selectedRoom.playerList[0].name}</span>
+          <span>VS</span>
+          <span className={styles.name}>{selectedRoom.playerList[1].name}</span>
+        </div>
+        <div className={styles.sides}>
+          <span>{playerSide[selectedRoom.playerList[0].id]}</span>
+          <span className={isYourTurn ? styles.yourTurn : styles.otherTurn}>
+            {isYourTurn ? '你的回合' : '對方回合'}
+          </span >
+          <span>{playerSide[selectedRoom.playerList[1].id]}</span>
         </div>
       </div>
+      <div className={styles.content}>
+        <div className={styles.map}>
+          <GameMap />
+          <div className={styles.chesses}>
+            {chessMap()}
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}></div>
     </div>
   )
 };
